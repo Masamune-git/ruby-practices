66ff18f4e8ecb2260a5e2e36ad95cdc06132339f
gentosato <2396niisankyumu@gmail.com>
2021-02-16 22:41:30 +0900

    変数i→calendar_date_numへの名称変更、範囲を判定する条件式をRange#include?へ変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,38 +1,38 @@
 #!/usr/bin/env ruby
 require 'optparse'
 require "date"
+options = ARGV.getopts('m:y:')
 
 # 変数iが正の場合その数字をカレンダーの日付として出力し、負または最終日よりも大きい数の場合はスペースを出力する
 def main(month,year)
   last_day = Date.new(year,month,-1).day
   first_wday = Date.new(year,month,1).wday
-  i = 1 - first_wday
+  calendar_date_num = 1 - first_wday
   printf("%8s", "#{month}月")
   printf("%5s", "#{year}")
   puts
   puts "日 月 火 水 木 金 土"
   6.times do
     7.times do
-      if month == Date.today.month && year == Date.today.year && i == Date.today.day
-        printf("%3s", "\e[7;7m#{i}\e[0m ")
-      elsif i <= 0 || i > last_day 
+      if month == Date.today.month && year == Date.today.year && calendar_date_num == Date.today.day
+        printf("%3s", "\e[7;7m#{calendar_date_num}\e[0m ")
+      elsif calendar_date_num <= 0 || calendar_date_num > last_day 
         printf("%3s",  "")
       else
-        printf("%3s", "#{i} ")
+        printf("%3s", "#{calendar_date_num} ")
       end
-        i += 1
+        calendar_date_num += 1
     end
     print "\n"
   end
 end
 
-options = ARGV.getopts('m:y:')
 month = options["m"].to_i
 year = options["y"].to_i
 default_month = Date.today.month
 default_year = Date.today.year
 
-if 1 <= month && month <= 12 && 1970 <= year && year <=2100
+if (1 .. 12).include?(month) && (1970 .. 2100).include?(year)
   #変数iはカレンダーの初日(1日)をi=1とした場合に最初の週の日曜日に入る相対的な数である
   main(month,year)  
 else
