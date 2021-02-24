c0f8d2e2bc86fe2ff502f988e70df06a84d5d08e
gentosato <2396niisankyumu@gmail.com>
2021-02-25 08:36:04 +0900

    コメントの追加とline50の処理の簡素化

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -4,14 +4,16 @@ score = ARGV[0]
 
 scores = score.split(',')
 shots = []
+
+# 数字に変換
 scores.each do |s|
-  if shots.size >= 18
+  if shots.size >= 18 # 10F目に三回投げていた場合strikeは10のまま
     if s == 'X'
       shots << 10
     else
       shots << s.to_i
     end
-  elsif s == 'X'
+  elsif s == 'X' # strikeの場合２投目を0にする
     shots << 10
     shots << 0
   else
@@ -19,15 +21,17 @@ scores.each do |s|
   end
 end
 
+# ２投ごとに1Fとして分割
 frames = []
 shots.each_slice(2) do |s|
   frames << s
 end
 point = 0
 
+# 点数計算
 9.times do |i|
   if frames[i][0] == 10 # strike
-    if frames[i + 1][0] == 10 && i != 8
+    if frames[i + 1][0] == 10 && i != 8 # 2F連続でstrikeだった場合
       point += 10 + 10 + frames[i + 2][0]
     else
       point += 10 + frames[i + 1][0] + frames[i + 1][1]
@@ -39,11 +43,10 @@ point = 0
   end
 end
 
+# 10F目は合計を足すだけ
 if frames.size == 10
   point += frames[9].sum
-else
-  (frames.size - 9).times do |i|
-    point += frames[frames.size - 1 - i].sum
-  end
+else # 10F目に三回投げていた場合
+  point += frames[9].sum + frames[10].sum
 end
 puts point
