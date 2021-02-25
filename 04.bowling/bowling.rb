2467ac14f2e520cb1fe2048eb12758568bda399c
gentosato <2396niisankyumu@gmail.com>
2021-02-25 10:58:49 +0900

    line13を三項演算子に変更、line31,46ni

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -10,11 +10,7 @@ shots = []
 # 数字に変換
 scores.each do |s|
   if shots.size >= 18 # 10F目に三回投げていた場合strikeは10のまま
-    if s == 'X'
-      shots << 10
-    else
-      shots << s.to_i
-    end
+    shots << (s == 'X' ? 10 : s.to_i)
   elsif s == 'X' # strikeの場合２投目を0にする
     shots << 10
     shots << 0
@@ -32,23 +28,25 @@ point = 0
 
 # 点数計算
 9.times do |i|
-  if frames[i][0] == 10 # strike
-    if frames[i + 1][0] == 10 && i != 8 # 2F連続でstrikeだった場合
-      point += 10 + 10 + frames[i + 2][0]
+  point +=
+    if frames[i][0] == 10 # strike
+      if frames[i + 1][0] == 10 && i != 8 # 2F連続でstrikeだった場合
+        10 + 10 + frames[i + 2][0]
+      else
+        10 + frames[i + 1][0] + frames[i + 1][1]
+      end
+    elsif frames[i].sum == 10 # spare
+      10 + frames[i + 1][0]
     else
-      point += 10 + frames[i + 1][0] + frames[i + 1][1]
+      frames[i].sum
     end
-  elsif frames[i].sum == 10 # spare
-    point += 10 + frames[i + 1][0]
-  else
-    point += frames[i].sum
-  end
 end
 
 # 10F目は合計を足すだけ
-if frames.size == 10
-  point += frames[9].sum
-else # 10F目に三回投げていた場合
-  point += frames[9].sum + frames[10].sum
-end
+point +=
+  if frames.size == 10
+    frames[9].sum
+  else # 10F目に三回投げていた場合
+    frames[9].sum + frames[10].sum
+  end
 puts point
