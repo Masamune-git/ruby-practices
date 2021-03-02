ad344ac5c258d5465eaadbff9c383fdef063719c
gentosato <2396niisankyumu@gmail.com>
2021-03-02 17:42:42 +0900

    test

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -2,51 +2,42 @@
 
 # !/usr/bin/env ruby
 
-score = ARGV[0]
-
-scores = score.split(',')
-shots = []
+scores = ARGV[0].split(',')
 
 # 数字に変換
-scores.each do |s|
-  if shots.size >= 18 # 10F目に三回投げていた場合strikeは10のまま
-    shots << (s == 'X' ? 10 : s.to_i)
-  elsif s == 'X' # strikeの場合２投目を0にする
+shots = []
+NINTH_FRAME_ENDED = 18
+
+scores.each do |score|
+  if score == 'X' # strikeの処理
     shots << 10
-    shots << 0
+    shots << 0 if shots.size < NINTH_FRAME_ENDED # 1~9F目でstrikeの場合２投目を0にする
   else
-    shots << s.to_i
+    shots << score.to_i
   end
 end
 
 # ２投ごとに1Fとして分割
-frames = []
-shots.each_slice(2) do |s|
-  frames << s
-end
-point = 0
+frames = shots.each_slice(2).to_a
 
 # 点数計算
-9.times do |i|
+point = 0
+9.times do |frame_no|
   point +=
-    if frames[i][0] == 10 # strike
-      if frames[i + 1][0] == 10 && i != 8 # 2F連続でstrikeだった場合
-        10 + 10 + frames[i + 2][0]
+    if frames[frame_no][0] == 10 # strike
+      if frames[frame_no + 1][0] == 10 && frame_no != 8 # 2F連続でstrikeだった場合
+        10 + 10 + frames[frame_no + 2][0]
       else
-        10 + frames[i + 1][0] + frames[i + 1][1]
+        10 + frames[frame_no + 1].sum
       end
-    elsif frames[i].sum == 10 # spare
-      10 + frames[i + 1][0]
+    elsif frames[frame_no].sum == 10 # spare
+      10 + frames[frame_no + 1][0]
     else
-      frames[i].sum
+      frames[frame_no].sum
     end
 end
 
 # 10F目は合計を足すだけ
-point +=
-  if frames.size == 10
-    frames[9].sum
-  else # 10F目に三回投げていた場合
-    frames[9].sum + frames[10].sum
-  end
+point += frames[9..10].flatten.sum
+
 puts point
