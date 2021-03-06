65663f6fc5b64ba337a6db5c34fed916aecd2b61
gentosato <2396niisankyumu@gmail.com>
2021-03-06 19:46:14 +0900

    06.wcディレクトリにwc.rbファイルを追加いたしました

---
+++ 06.wc/wc.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+# !/usr/bin/env ruby
+
+require 'optparse'
+
+def file_line_count(file)
+  File.read(file).count("\n")
+end
+
+def file_word_count(file)
+  File.read(file).scan(/\s+/).count
+end
+
+def file_bite_size(file)
+  File.lstat(file).size
+end
+
+def total_file_line_count(files)
+  files.sum { |file| file_line_count(file) }
+end
+
+def total_file_word_count(files)
+  files.sum { |file| file_word_count(file) }
+end
+
+def total_file_bite_size(files)
+  files.sum { |file| file_bite_size(file) }
+end
+
+files = ARGV
+opt = OptionParser.new
+option = []
+opt.on('-l') do
+  option << 'l'
+  files.shift
+end
+opt.parse(ARGV)
+
+if files.empty? # 標準入力から受け取る場合の処理
+  std_input = readlines.join
+  printf '% *s', 8, std_input.count("\n").to_s
+  if option.include?('l') == false
+    printf '% *s', 8, std_input.scan(/\s+/).count.to_s
+    printf '% *s', 8, std_input.size.to_s
+  end
+  print "\n"
+end
+
+if option.include?('l') #-lオプションの処理
+  files.each do |file|
+    printf '% *s', 8, file_line_count(file).to_s
+    puts format '% s', " #{file}"
+  end
+else
+  files.each do |file|
+    printf '% *s', 8, file_line_count(file).to_s
+    printf '% *s', 8, file_word_count(file).to_s
+    printf '% *s', 8, file_bite_size(file).to_s
+    puts format '% s', " #{file}"
+  end
+end
+
+# 複数ファイルを読み取った場合の処理
+if files.size >= 2 && option.include?('l') #-lオプションの処理
+  printf '% *s', 8, total_file_line_count(files).to_s
+  puts format '% s', ' total'
+elsif files.size >= 2 && option.include?('l') == false
+  printf '% *s', 8, total_file_line_count(files).to_s
+  printf '% *s', 8, total_file_word_count(files).to_s
+  printf '% *s', 8, total_file_bite_size(files).to_s
+  puts format '% s', ' total'
+end
